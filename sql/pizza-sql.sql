7e8be5e7d957afaf32c908a1c31b8c2decf9b632
rubayad <133543185+rubayad@users.noreply.github.com>
2023-10-06 22:02:42 -0400

    Add files via upload

---
+++ sql/pizza-sql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE pizzas (
+  id int not null auto_increment,
+  pizza varchar(255) NOT NULL,
+  fname varchar(255) NOT NULL,
+  lname varchar(255) NOT NULL,
+  phone varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  primary key (id)
+);
